b3ad080fa3fccf41699e13321657264c4fb4c9aa
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-01-14 16:32:05 -0800

    added email

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
 	$('#contact').on('click', function(){
 		$('#form1').toggleClass('hidden');
+		$("#overlay").removeClass("hidden");
+	});
 
-	
+	$("#overlay").on('click', function(e){
+		if ($(e.target).is('#overlay')){
+			$("#overlay").toggleClass("hidden");
+			$('#form1').toggleClass('hidden');
+		}
+	})
+
+	$("#form1").on("submit", function(e) {
+		e.preventDefault();
+		$("#overlay").toggleClass("hidden");
+		$('#form1').toggleClass('hidden');
+	  $.ajax({
+	      url: '//formspree.io/thomasshannon1117@yahoo.com', 
+	      method: 'POST',
+	      data: {
+	      	name: $('#name').val(),
+	      	email:$('#email').val(),
+	      	message: $('#message').val()},
+	      dataType: 'json'
+	  });
+	  
 	});
 });
 
